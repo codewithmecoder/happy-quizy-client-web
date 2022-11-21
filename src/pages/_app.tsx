65a839a3fc8f517d6429acfd03971db20a5f972b
cheanith2909 <sokcheanith1031@hotmail.com>
2022-11-21 14:33:06 +0700

    fix: deps

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,19 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
-import '../styles/globals.css';
+
+import '../../styles/globals.css';
+import Navbar from '../components/Navbar';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="bg-zinc-800 min-h-screen min-w-full">
+        <Navbar userData={pageProps.userData} />
+        <Component {...pageProps} />
+      </div>
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp;
